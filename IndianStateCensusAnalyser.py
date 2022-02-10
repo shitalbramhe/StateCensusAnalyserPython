ac5230fa20c54df68e6f5ee0145e649a7fe1bde6
shital bramhe <shitalbramhe@gmail.com>
2022-02-10 11:24:23 +0530

    [Shital] Add : Test to check file is not exist then returns custom exception

--- IndianStateCensusAnalyser.py
+++ IndianStateCensusAnalyser.py
@@ -2,38 +2,57 @@
 @Author: Shital Bajait
 @Date: 08-02-2022 13:12:00
 @Last Modified by: Shital Bajait 
-@Last Modified time: 08-02-2022 13:12:00
-@Title : To load CSV Data
+@Last Modified time: 10-02-2022 10:40:00
+@Title : Given the State CSV File if incorrect Returns a custom Exception
 """
 
 import csv
 
-class StateCensusAnalyser:
+class StateCensusAnalyser():
     def state_census():
+        """
+            Description:
+                Function to load StateCensusData file
+            Parameter:
+                None
+            Return:
+                None
+        """
         with open("E:\CFP\StateCensusAnalyserPython\StateCensusData.csv","r") as data:
             statecensus = csv.reader(data, delimiter=',')
             for i in statecensus:
                 print(i)
 
     def count_number_records():
+        """
+            Description:
+                Function to count number of records
+            Parameter:
+                None
+            Return:
+                rows of records
+        """
         with open("E:\CFP\StateCensusAnalyserPython\StateCensusData.csv") as data:
             statecensus = csv.reader(data, delimiter=',')
             return len(list(statecensus))
 
-class CSVStates:
-    def state_code():
-        with open("E:\CFP\StateCensusAnalyserPython\StateCode.csv", "r") as data:
-            statecode = csv.reader(data, delimiter=',')
-            for i in statecode:
-                print(i)
-
-    def count_number_records():
-        with open("E:\CFP\StateCensusAnalyserPython\StateCode.csv") as data:
-            statecode = csv.reader(data, delimiter=',')
-            return (len(list(statecode)))
+    def check_file():
+        """
+            Description:
+                Function to ccheck file is exist or not
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            f = open("E:\CFP\StateCensusAnalyserPython\StateCensusData.csv")
+            f.close()
+            return "StateCensusData.csv"
+        except Exception:
+            print("Sorry file does not exist")
 
 if __name__ == '__main__':
     StateCensusAnalyser.state_census()
     print(StateCensusAnalyser.count_number_records())
-    CSVStates.state_code()
-    print(CSVStates.count_number_records())
+    StateCensusAnalyser.check_file()
--- Test_state_census.py
+++ Test_state_census.py
@@ -3,9 +3,14 @@ import IndianStateCensusAnalyser
 class Test(unittest.TestCase):
     def test_number_records_matches(self):
         result = IndianStateCensusAnalyser.StateCensusAnalyser.count_number_records()
-        print(result)
         expected = 30
         self.assertEqual(expected, result)
+
+    def test_file(self):
+        result = IndianStateCensusAnalyser.StateCensusAnalyser.check_file()
+        expected = "StateCensusData.csv"
+        self.assertEqual(expected, result)
+        
 
 if __name__ =='__main__':
     unittest.main()
