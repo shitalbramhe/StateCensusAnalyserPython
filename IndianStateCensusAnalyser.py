1f95e09d34fecd5164feb2f7368858e5f5dfd7cc
shital bramhe <shitalbramhe@gmail.com>
2022-02-10 12:13:42 +0530

    [Shital] refactor : test to check number of records

--- IndianStateCensusAnalyser.py
+++ IndianStateCensusAnalyser.py
@@ -3,37 +3,40 @@
 @Date: 08-02-2022 13:12:00
 @Last Modified by: Shital Bajait 
 @Last Modified time: 08-02-2022 13:12:00
-@Title : To load CSV Data
+@Title : Check to ensure the Number of Record matches
 """
 
 import csv
 
 class StateCensusAnalyser:
     def state_census():
+        """
+            Description:
+                Function to load StateCensusData file
+            Parameter:
+                None
+            Return:
+                None
+        """
         with open("E:\CFP\StateCensusAnalyserPython\StateCensusData.csv","r") as data:
             statecensus = csv.reader(data, delimiter=',')
             for i in statecensus:
                 print(i)
 
     def count_number_records():
+        """
+            Description:
+                Function to count number of records
+            Parameter:
+                None
+            Return:
+                rows of records
+        """
         with open("E:\CFP\StateCensusAnalyserPython\StateCensusData.csv") as data:
             statecensus = csv.reader(data, delimiter=',')
             return len(list(statecensus))
 
-class CSVStates:
-    def state_code():
-        with open("E:\CFP\StateCensusAnalyserPython\StateCode.csv", "r") as data:
-            statecode = csv.reader(data, delimiter=',')
-            for i in statecode:
-                print(i)
-
-    def count_number_records():
-        with open("E:\CFP\StateCensusAnalyserPython\StateCode.csv") as data:
-            statecode = csv.reader(data, delimiter=',')
-            return (len(list(statecode)))
-
 if __name__ == '__main__':
     StateCensusAnalyser.state_census()
     print(StateCensusAnalyser.count_number_records())
-    CSVStates.state_code()
-    print(CSVStates.count_number_records())
+    
