ba7e02e7411f832a64addeb94d4c53dc17d64a54
shital bramhe <shitalbramhe@gmail.com>
2022-02-13 22:51:24 +0530

    [Shital] add : sad and happy test cases for new csv file

--- Merge_two_csv_file.py
+++ Merge_two_csv_file.py
@@ -7,7 +7,7 @@
 """
 import csv
 
-class StateCensusData():
+class StateCensusData(Exception):
 
     def state_census_code_combine():
             """
@@ -20,7 +20,7 @@ class StateCensusData():
             """
             with open("E:\CFP\StateCensusAnalyserPython\StateCensusData.csv","r") as f1:
                 csv_reader1 = csv.reader(f1, delimiter=',')
-                fields = ['State', 'Population', 'TIN', 'StateCode']
+                fields = ['State','Population','TIN','StateCode']
                 with open('resultfile.csv','w', newline='') as f3:
                     csv_writer = csv.writer(f3, delimiter=',')
                     csv_writer.writerow(fields)
@@ -29,8 +29,99 @@ class StateCensusData():
                             csv_reader2 = csv.reader(f2, delimiter=',')
                             for statecode_row in csv_reader2:
                                 if statecensus_row[0] == statecode_row[1]:
-                                    csv_writer.writerow({statecensus_row[0]+" "+statecensus_row[1]+" "+statecode_row[2]+" "+statecode_row[3]})
+                                    csv_writer.writerow({statecensus_row[0]+","+statecensus_row[1]+","+statecode_row[2]+","+statecode_row[3]})
 
+    def count_number_records():
+        """
+            Description:
+                Function to count number of records
+            Parameter:
+                None
+            Return:
+                rows of records
+        """
+        with open('resultfile.csv') as data:
+            result_file = csv.reader(data,delimiter=',')
+            return(len(list(result_file)))
+    
+    def check_file():
+        """
+            Description:
+                Function to ccheck file is exist or not
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            f = open('resultfile.csv')
+            f.close()
+            return "resultfile.csv"
+        except Exception:
+            print("Sorry file does not exist")
+    
+    def check_file_extension():
+        """
+            Description:
+                Function to check csv file exists or not
+            Parameter:
+                None
+            Return:
+                .csv
+        """
+        try:
+            file = "resultfile.csv"
+            result = file.endswith(".csv")
+            if result:
+                return ".csv"
+            else:
+                raise(StateCensusData)
+        except StateCensusData:
+            print("Sorry! CSV file does not exist")
+
+    def check_delimiter():
+        """
+            Description:
+                Function to check delimiter is correct or not
+            Parameter:
+                None
+            Return:
+                delimiter
+        """
+        try:
+            with open('resultfile.csv') as data:
+                dialect = csv.Sniffer().sniff(data.read())
+                if dialect.delimiter == ',':
+                    return dialect.delimiter
+                else:
+                    raise(StateCensusData)
+        except StateCensusData:
+                print("Incorrect delimiter found")
+
+    def check_header():
+        """
+            Description:
+                Function to check header is present or not
+            Parameter:
+                None
+            Return:
+                .csv
+        """
+        try:
+            with open('resultfile.csv') as data:
+                headers = csv.Sniffer().has_header(data.read())
+                if headers:
+                    return headers
+                else:
+                    raise(StateCensusData)
+        except StateCensusData:
+            print("headers Not found")
 
 if __name__ == '__main__':
     StateCensusData.state_census_code_combine()
+    StateCensusData.count_number_records()
+    StateCensusData.check_file()
+    StateCensusData.check_file_extension()
+    StateCensusData.check_delimiter()
+    StateCensusData.check_header()
+    
---
+++ test_resultfile.py
@@ -0,0 +1,55 @@
+import unittest
+import Merge_two_csv_file
+class Test(unittest.TestCase):
+    def test_number_records_matches(self):
+        result = Merge_two_csv_file.StateCensusData.count_number_records()
+        expected = 30
+        self.assertEqual(expected, result)
+
+    def test_number_records_not_matches(self):
+        result = Merge_two_csv_file.StateCensusData.count_number_records()
+        expected = 31
+        self.assertEqual(expected, result)
+
+    def test_file_exist(self):
+        result = Merge_two_csv_file.StateCensusData.check_file()
+        expected = "resultfile.csv"
+        self.assertEqual(expected, result)
+    
+    def test_file_notexist(self):
+        result = Merge_two_csv_file.StateCensusData.check_file()
+        expected = "file.csv"
+        self.assertEqual(expected, result)
+    
+    def test_file_extension(self):
+        result = Merge_two_csv_file.StateCensusData.check_file_extension()
+        expected = ".csv"
+        self.assertEqual(expected, result)
+
+    def test_file_extension_other(self):
+        result = Merge_two_csv_file.StateCensusData.check_file_extension()
+        expected = ".json"
+        self.assertEqual(expected, result)
+
+    def test_check_delimiter(self):
+        result = Merge_two_csv_file.StateCensusData.check_delimiter()
+        expected = ','
+        self.assertEqual(expected, result)
+
+    def test_check_delimiter_not(self):
+        result = Merge_two_csv_file.StateCensusData.check_delimiter()
+        expected = '/'
+        self.assertEqual(expected, result)    
+    
+    def test_check_header(self):
+        result = Merge_two_csv_file.StateCensusData.check_header()
+        expected = True
+        self.assertEqual(expected, result)
+
+    def test_check_header_not(self):
+        result = Merge_two_csv_file.StateCensusData.check_header()
+        expected = False
+        self.assertEqual(expected, result)
+
+if __name__ =='__main__':
+    unittest.main()
